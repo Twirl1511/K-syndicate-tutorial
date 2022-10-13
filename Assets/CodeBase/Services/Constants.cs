ff3bca4b0f02eaf8bbfdfe92ed1ec1c58594eee9
Mikhail Larkin <Twirl511@yandex.ru>
2022-10-13 16:48:58 +0500

    !F update project

---
+++ Assets/CodeBase/Services/Constants.cs
@@ -0,0 +1,7 @@
+﻿namespace CodeBase.Services
+{
+    public static class Constants
+    {
+        public const float Epsilon = 0.001f;
+    }
+}
---
+++ Assets/CodeBase/Services/Input/IInputService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace CodeBase.Services.Input
+{
+    public interface IInputService
+    {
+        Vector2 Axis { get; }
+
+        bool IsAttackButtonUp();
+    }
+}

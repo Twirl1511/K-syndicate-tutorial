c1b2e5608cfc3061dbe3556fbbf0c6a1b09f4198
Mikhail Larkin <Twirl511@yandex.ru>
2022-10-14 19:55:28 +0500

    !R

---
+++ Assets/CodeBase/Infrastructure/BootstrapState.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CodeBase.Infrastructure
+{
+    public  class  BootstrapState : IState
+    {
+        public void Enter()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Exit()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
